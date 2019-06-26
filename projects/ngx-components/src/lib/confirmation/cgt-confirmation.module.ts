e1b6586f027e4d4951ebc2c751a682cf853fdf91
Craig Gaskill <craig.gaskill@gmail.com>
2019-06-25 21:30:46 -0500

    Minor cleanup for confirmation dialog.

--- projects/ngx-components/src/lib/confirmation/cgt-confirmation.module.ts
+++ projects/ngx-components/src/lib/confirmation/cgt-confirmation.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatButtonModule, MatDialogModule} from '@angular/material';
 
 import {CgtConfirmationComponent} from './cgt-confirmation.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   declarations: [
     CgtConfirmationComponent
