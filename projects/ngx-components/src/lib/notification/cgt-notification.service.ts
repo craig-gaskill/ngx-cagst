c7d0e250009cf7e144a872a83866f737d06e755d
Craig Gaskill <craig.gaskill@gmail.com>
2019-07-08 06:32:08 -0500

    Minor cleanup.

--- projects/ngx-components/src/lib/notification/cgt-notification.service.ts
+++ projects/ngx-components/src/lib/notification/cgt-notification.service.ts
@@ -5,23 +5,23 @@ import {MatSnackBar} from '@angular/material';
   providedIn: 'root'
 })
 export class CgtNotificationService {
-  private readonly DEFAULT_DURATION = 2000;
+  private readonly DEFAULT_DURATION = 2500;
 
   constructor(private _snackBar: MatSnackBar) { }
 
-  public inform(message: string, actions?: string, duration?: number) {
-    this._snackBar.open(message, actions, {panelClass: 'notify-inform', duration: duration ? duration : this.DEFAULT_DURATION});
+  public inform(message: string, actions?: string, duration = this.DEFAULT_DURATION) {
+    this._snackBar.open(message, actions, {panelClass: 'notify-inform', duration});
   }
 
-  public warn(message: string, actions?: string, duration?: number) {
-    this._snackBar.open(message, actions, {panelClass: 'notify-warn', duration: duration ? duration : this.DEFAULT_DURATION});
+  public warn(message: string, actions?: string, duration = this.DEFAULT_DURATION) {
+    this._snackBar.open(message, actions, {panelClass: 'notify-warn', duration});
   }
 
-  public success(message: string, actions?: string, duration?: number) {
-    this._snackBar.open(message, actions, {panelClass: 'notify-success', duration: duration ? duration : this.DEFAULT_DURATION});
+  public success(message: string, actions?: string, duration = this.DEFAULT_DURATION) {
+    this._snackBar.open(message, actions, {panelClass: 'notify-success', duration});
   }
 
-  public failure(message: string, actions?: string, duration?: number) {
-    this._snackBar.open(message, actions, {panelClass: 'notify-failure', duration: duration ? duration : this.DEFAULT_DURATION});
+  public failure(message: string, actions?: string, duration = this.DEFAULT_DURATION) {
+    this._snackBar.open(message, actions, {panelClass: 'notify-failure', duration});
   }
 }
